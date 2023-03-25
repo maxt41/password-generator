fbdb4d354d7380c07a92274e56a7361828147c89
Max <41212294+maxt41@users.noreply.github.com>
2023-03-25 20:00:08 +0000

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <h1 style={{fontWeight: '900', fontSize: '2.5vw', maxWidth: '80%', margin: 'auto', lineHeight: '56px'}}>Instantly generate a secure, random password with the LastPass online tool</h1>
+    <h1 style={{fontWeight: '900', fontSize: '3rem', maxWidth: '80%', margin: 'auto', lineHeight: '56px'}}>Instantly generate a secure, random password with the LastPass online tool</h1>
   )
 }
 
